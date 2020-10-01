939309f07097f2f32ad376ad567b9e61bc24d124
ergouwang <330035385@qq.com>
2020-10-02 06:56:52 +0800

    页面底部导航完成

--- lib/navigator/tab_navigator.dart
+++ lib/navigator/tab_navigator.dart
@@ -11,6 +11,12 @@ class TabNavigator extends StatefulWidget{
 
 //在定义内部类的时候我们需要在前面加上一个下划线
 class _TabNavigatorState extends State<TabNavigator>{
+  //未选中时的颜色
+  final _defaultColor = Colors.grey;
+  //选中时的颜色
+  final _activeColor = Colors.blue;
+  //当前选中的 index
+  int _currentIndex = 0;
   final PageController _controller = PageController(
     //初始化的时候显示第0个tab
     initialPage: 0,
@@ -28,6 +34,47 @@ class _TabNavigatorState extends State<TabNavigator>{
           MyPage(),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index){
+          _controller.jumpToPage(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type:BottomNavigationBarType.fixed ,
+        items: [
+          BottomNavigationBarItem(
+            icon:Icon( Icons.home, color:_defaultColor , ),
+            activeIcon: Icon( Icons.home, color:_activeColor, ),
+            title:Text('首页',style:TextStyle(
+              color: _currentIndex != 0 ? _defaultColor : _activeColor
+            ) ,)
+          ),
+          BottomNavigationBarItem(
+              icon:Icon( Icons.search, color:_defaultColor , ),
+              activeIcon: Icon( Icons.search, color:_activeColor, ),
+              title:Text('搜索',style:TextStyle(
+                  color: _currentIndex != 1 ? _defaultColor : _activeColor
+              ) ,)
+          ),
+          BottomNavigationBarItem(
+              icon:Icon( Icons.camera, color:_defaultColor , ),
+              activeIcon: Icon( Icons.camera, color:_activeColor, ),
+              title:Text('旅拍',style:TextStyle(
+                  color: _currentIndex != 2 ? _defaultColor : _activeColor
+              ) ,)
+          ),
+          BottomNavigationBarItem(
+              icon:Icon( Icons.account_circle, color:_defaultColor , ),
+              activeIcon: Icon( Icons.account_circle, color:_activeColor, ),
+              title:Text('我的',style:TextStyle(
+                  color: _currentIndex != 3 ? _defaultColor : _activeColor
+              ) ,)
+          ),
+        ]
+      ),
+
 
     );
   }
